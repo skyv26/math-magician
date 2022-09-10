0c9854eccaa2e1c4acc51ad493e286f79589c75c
skyv26 <dev.aakashv@gmail.com>
2022-09-10 15:14:19 +0530

    import `calculate.js` and add `event-listener` on buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 import './Calculator.css';
 
-class Calculator extends PureComponent {
+class Calculator extends Component {
   constructor(props) {
     super(props);
     this.keysArray = [
@@ -11,18 +12,30 @@ class Calculator extends PureComponent {
       '1', '2', '3', '+',
       '0', '.', '=',
     ];
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
+
+  buttonClickHandler = (e) => {
+    const { textContent } = e.target;
+    console.log(calculate(this.state, textContent));
   }
 
   render() {
+    const { total } = this.state;
     return (
       <div className="calculator">
         <div className="display">
-          <p className="calculation">0</p>
+          <p className="calculation">{total ?? 0}</p>
         </div>
         <div className="keypad">
           {
             // eslint-disable-next-line react/no-array-index-key
-            this.keysArray.map((each, index) => <button key={index} type="button" className={`button ${each === '0' ? 'zero-btn' : ''} ${['+', '-', 'x', '÷', '='].includes(each) ? 'operation-btn' : ''}`}>{each}</button>)
+            this.keysArray.map((each, index) => (<button key={index} type="button" className={`button ${each === '0' ? 'zero-btn' : ''} ${['+', '-', 'x', '÷', '='].includes(each) ? 'operation-btn' : ''}`.trim()} onClick={(e) => { this.buttonClickHandler(e); }}>{each}</button>))
           }
         </div>
       </div>
