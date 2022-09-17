532782147907e2ba33d64dff8e688f62a7ed37de
skyv26 <dev.aakashv@gmail.com>
2022-09-17 19:46:56 +0530

    Update `Header` component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <>
-    </>
-  );
-};
+const Header = () => (
+  <header>
+    <h1>Math Magicians</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Calculator</li>
+        <li>Quote</li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
