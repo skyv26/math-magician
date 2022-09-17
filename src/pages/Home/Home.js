b9388a8964e04b2f11e7d83318be056848d1fdf3
skyv26 <dev.aakashv@gmail.com>
2022-09-17 20:50:02 +0530

    Add `content` into `[Home]` component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Home = () => (
-  <></>
+  <section className="section section-home">
+    <h2 className="section-heading">Welcome to our page!</h2>
+    <p className="desc">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+      modi accusamus. Nostrum autem et labore eveniet tempora voluptatem,
+      eligendi aliquam, fugit ipsum nisi accusamus, itaque architecto nemo omnis nobis distinctio?
+    </p>
+    <p className="desc">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+      modi accusamus. Nostrum autem et labore eveniet tempora voluptatem,
+      eligendi aliquam, fugit ipsum nisi accusamus, itaque architecto nemo omnis nobis distinctio?
+    </p>
+  </section>
 );
 
 export default Home;
