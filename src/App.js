1b338c9fcdbccc1cbf37bd308f54778e34f2198c
skyv26 <dev.aakashv@gmail.com>
2022-09-17 20:19:30 +0530

    Add `Route Path` in `App` component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 // import Calculator from './components/Calculator';
 import Header from './components/Header';
+import Home from './pages/Home/Home';
+import Quote from './pages/Quote/Quote';
 
 const App = () => (
   <>
     <Header />
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/calculator" element={<App />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
     {/* <Calculator /> */}
   </>
 );
